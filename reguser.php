756627fe8b5928fdd88209f86f5dc33907b337bd
Furqan <furqode@gmail.com>
2023-04-10 06:30:10 +0100

    code that inserts data into database

--- reguser.php
+++ reguser.php
@@ -8,6 +8,17 @@ $db = "login";
 $con = mysqli_connect($servername, $username, $password, $db);
 
 if (isset($_POST["submit"])) {
-    echo "cringe";
+    $user = $_POST["username"];
+    $userpass = $_POST["userpass"];
+    $displayname = $_POST ["displayname"];
+    $useremail = $_POST ["useremail"];
+
+    $sql = "INSERT INTO users (username, userpass, displayname, useremail) VALUES ('$user', '$userpass', '$displayname', '$useremail')";
+    
+    if (mysqli_query($con, $sql)) {
+        echo "Data stored successfully!";
+    } else {
+        echo "Some error occured!";
+    }
 }
 ?>
