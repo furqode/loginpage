4789f74f7557764fa08d3531963a346d25a7fab8
Furqan <furqode@gmail.com>
2023-04-18 01:41:34 +0100

    add htmlspecialchar to user credentials

--- reguser.php
+++ reguser.php
@@ -8,10 +8,10 @@ $db = "login";
 $con = mysqli_connect($servername, $username, $password, $db);
 
 if (isset($_POST["submit"])) {
-    $user = $_POST["username"];
-    $userpass = $_POST["userpass"];
-    $displayname = $_POST ["displayname"];
-    $useremail = $_POST ["useremail"];
+    $user = htmlspecialchar($_POST["username"]);
+    $userpass = htmlspecialchar($_POST["userpass"]);
+    $displayname = htmlspecialchar($_POST ["displayname"]);
+    $useremail = htmlspecialchar($_POST ["useremail"]);
 
     $sql = "INSERT INTO users (username, userpass, displayname, useremail) VALUES ('$user', '$userpass', '$displayname', '$useremail')";
     
