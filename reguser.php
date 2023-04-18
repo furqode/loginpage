d6458c5fd06dbf28358075433282004a813c354d
Furqan <furqode@gmail.com>
2023-04-18 02:09:04 +0100

    fix error handling

--- reguser.php
+++ reguser.php
@@ -7,23 +7,57 @@ $db = "login";
 
 $con = mysqli_connect($servername, $username, $password, $db);
 
-$userErr = "";
-$passErr = "";
-$nameErr = "";
-$emailErr = "";
-
 if (isset($_POST["submit"])) {
-    $user = htmlspecialchars($_POST["username"]);
-    $userpass = htmlspecialchars($_POST["userpass"]);
-    $displayname = htmlspecialchars($_POST ["displayname"]);
-    $useremail = htmlspecialchars($_POST ["useremail"]);
 
-    $sql = "INSERT INTO users (username, userpass, displayname, useremail) VALUES ('$user', '$userpass', '$displayname', '$useremail')";
+// Form validation
+
+$errors = array();
+
+    if (empty($_POST["username"])) {
+
+        $errors[] = "Please enter your username.";
     
-    if (mysqli_query($con, $sql)) {
-        echo "Data stored successfully!";
-    } else {
-        echo "Some error occured!";
     }
+
+    if (empty($_POST["userpass"])) {
+    
+        $errors[] = "Please enter a valid username.";
+    
+    }
+
+    if (empty($_POST["displayname"])) {
+
+        $errors[] = "Please enter a valid Display Name.";
+    
+    }
+
+    if (empty($_POST["useremail"])) {
+
+        $errors[] = "Please enter a valid email.";
+
+    }
+
+    if (empty($errors)) {
+
+        $user = htmlspecialchars($_POST["username"]);
+        $userpass = htmlspecialchars($_POST["userpass"]);
+        $displayname = htmlspecialchars($_POST ["displayname"]);
+        $useremail = htmlspecialchars($_POST ["useremail"]);
+
+        $sql = "INSERT INTO users (username, userpass, displayname, useremail) VALUES ('$user', '$userpass', '$displayname', '$useremail')";
+    
+        if (mysqli_query($con, $sql)) {
+            echo "Data stored successfully!";
+        } else {
+            echo "Some error occured!";
+        }
+    }
+
+    
+    
+    
+    
+
+  
 }
 ?>
