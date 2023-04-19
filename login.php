ac2a4a73bd3965e3876e6caf1f57d1269a1576c3
Furqan <furqode@gmail.com>
2023-04-19 03:02:42 +0100

    verify the user and decrypt the password

--- login.php
+++ login.php
@@ -7,4 +7,30 @@ $db = "login";
 
 $con = mysqli_connect($servername, $username, $password, $db);
 
+if (isset($_POST['submit'])) {
+
+    $user = $_POST['username'];
+    $userpass = $_POST['userpass'];
+
+    // selecting the userpass from the users table
+
+    $sql = "SELECT userpass FROM users WHERE username='$user'";
+    
+    // fetching the user passwords and store them in an array
+    
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_assoc($result);
+
+    $hashedpass = $row['userpass'];
+
+    //decrypt the password and verify the user
+
+    if (password_verify($userpass, $hashedpass)) {
+        echo "welcome '$user'";
+    } else {
+        echo "error";
+    }
+
+}
+
 ?>
